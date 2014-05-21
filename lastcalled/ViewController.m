27abd5474bdeb054b4f02039ee7ea8e4768bc19d
david geijer <david.geijer@netlight.com>
2014-05-21 10:12:19 +0200

    Initial Commit

---
+++ lastcalled/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  lastcalled
+//
+//  Created by David Geijer on 21/05/14.
+//  Copyright (c) 2014 Witch Craft International. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
